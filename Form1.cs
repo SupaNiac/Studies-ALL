901136272baf28a424bb2759585041f0468f4d46
Luiz Henrique <luiz04faria@gmail.com>
2025-01-27 22:52:28 -0300

    Sql and stuff

---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Aula16._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Mensagem(string msg)
+        {
+            listBox1.Items.Add(msg);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SQL Server
+            //  SQL Connection
+            //      ConnectionString
+            //      Open()
+            //      Close()
+         
+
+            //  SQL Command
+            //SqlDataReader
+
+            var cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=EmpresaDb;
+                                  Integrated Security=True;Pooling=False;";
+
+            cn.Open();
+            Mensagem("Conexão aberta");
+            cn.Close();
+            Mensagem("Conexão fechada");
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=EmpresaDb;
+                                  Integrated Security=True;Pooling=False;";
+
+            var cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = "SELECT Nome FROM Cliente";
+
+            cn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string nome = reader["Nome"].ToString();
+                listBox1.Items.Add(nome);  
+             }
+            reader.Close();
+            cn.Close() ;
+        }
+    }
+}
