f758da9886a1c8b6a65c0b650a12d15237fd9fa6
Luiz Henrique <luiz04faria@gmail.com>
2025-02-04 17:31:19 -0300

    New buttons and functions

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -47,9 +50,9 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(13, 73);
+            this.listBox1.Location = new System.Drawing.Point(13, 74);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(775, 364);
+            this.listBox1.Size = new System.Drawing.Size(901, 364);
             this.listBox1.TabIndex = 1;
             // 
             // button2
@@ -58,15 +61,48 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(161, 40);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Comando";
+            this.button2.Text = "Select";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(388, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Insert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(572, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 40);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(755, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(161, 40);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(926, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
@@ -81,6 +117,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -68,5 +68,65 @@ namespace Aula16._2
             reader.Close();
             cn.Close() ;
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=EmpresaDb;
+                                  Integrated Security=True;Pooling=False;";
+
+            var cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = "INSERT INTO Cliente(Id,Nome,Email,Telefone) Values(@Id,@Nome,@Email,@Telefone)";
+            cmd.Parameters.AddWithValue("@Id", 25);
+            cmd.Parameters.AddWithValue("@Nome", "Silvio Santos");
+            cmd.Parameters.AddWithValue("@Telefone", "83598250");
+            cmd.Parameters.AddWithValue("@Email", "@aodkaokd@");
+            cn.Open();
+            Mensagem("Conexão aberta");
+            
+            int total = cmd.ExecuteNonQuery();
+            Mensagem("Registro incluido");
+
+            cn.Close();
+            Mensagem("Conexão fechada");
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=EmpresaDb;
+                                  Integrated Security=True;Pooling=False;";
+
+            var cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = "UPDATE Cliente SET Nome=@Nome Where Id=@Id";
+            cmd.Parameters.AddWithValue("@Id", 25);
+            cmd.Parameters.AddWithValue("@Nome", "Silvio Santos Santos");
+            cn.Open();
+            int total = cmd.ExecuteNonQuery();
+            cn.Close();
+            Mensagem("Registro alterado com sucesso!");
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=EmpresaDb;
+                                  Integrated Security=True;Pooling=False;";
+
+            var cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = "DELETE Cliente WHERE Id=@Id";
+            cmd.Parameters.AddWithValue("@Id", 25);
+            cn.Open();
+            int total = cmd.ExecuteNonQuery();
+            cn.Close();
+            Mensagem("Registro excluido com sucesso!");
+        }
     }
 }
